3c3b0dc4b78c224e77ec6907e1c4d10ef35652e8
ANANDHU KK <kkanandhu97@gmail.com>
2017-09-21 12:54:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,16 +4,11 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var article-one={
+var articleone={
     title:'article one/anandhu',
     heading:'article one',
     date:'sept 5 2017',
-    content: '<p1>
-                        Dulquer Salmaan (born 28 July 1986), also referred popularly as DQ, is an Indian film actor who appears predominantly in Malayalam films.The son of film actor Mammootty, Salmaan graduated with a bachelor's degree in Business Management from Purdue University, and worked as a business manager before pursuing a career in acting
-                    </p1>
-                    <p2>
-                        Salmaan married architect Amal Sufiya in 2011 with whom he has one daughter. Cited in the media as one of the most attractive Indian celebrities, Salmaan has also been recognised for his fashion style.[3][4] He owns several entrepreneurship ventures and promotes various social causes
-                    </p2> '
+    content: '<p1>Dulquer Salmaan (born 28 July 1986), also referred popularly as DQ, is an Indian film actor who appears predominantly in Malayalam films.The son of film actor Mammootty, Salmaan graduated with a bachelors degree in Business Management from Purdue University, and worked as a business manager before pursuing a career in acting </p1><p2>Salmaan married architect Amal Sufiya in 2011 with whom he has one daughter. Cited in the media as one of the most attractive Indian celebrities, Salmaan has also been recognised for his fashion style.[3][4] He owns several entrepreneurship ventures and promotes various social causes</p2>'
     
 };
 function createtemplate(data){
@@ -21,41 +16,7 @@ function createtemplate(data){
     var heading=data.heading;
     var date=data.date;
     var content=data.content;
-var htmltemplate='
-    
-    <!DOCTYPE html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <link href="/ui/style.css" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-    </head>
-    <body>
-            <div class="container">
-        
-                <div>
-                    <a href="/">Home</a>
-                </div>
-                <br/>
-                
-                 <h1>
-                   ${heading}
-                </h1>
-                
-                 <div>
-                    ${date}
-                </div>
-                
-                <div>
-                    ${content}
-                </div>
-            </div>
-    </body>
-   
-
-</html>';
+var htmltemplate=' <!DOCTYPE html><head> ${title}</title><link href="/ui/style.css" rel="stylesheet" /><meta name="viewport" content="width=device-width, initial-scale=1.0"></head><body><div class="container"><div><a href="/">Home</a></div><br/><h1> ${heading}</h1><div>${date}</div><div>${content} </div></div></body></html>';
 return htmltemplate
 }
 
@@ -71,7 +32,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/article-one', function (req, res) {
-  res.send(createtemplate(article-one));
+  res.send(createtemplate(articleone));
 });
 app.get('/article-two', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
