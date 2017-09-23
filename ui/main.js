1d10931afd1c768ee85daaf186c949cd20dbd484
ANANDHU KK <kkanandhu97@gmail.com>
2017-09-23 15:23:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,12 +7,11 @@ submit.onclick=function(){
     var names=['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<names.length;i++){
-        list='<li>'+ i +'</li>';
-       var ul=document.getElementById('namelist');
-   ul.innerHTML=list;
+        list=name[i] +'<br/>';
+      
   
     }
-   // var ul=document.getElementById('namelist');
-  // ul.innerHTML=list;
+    var ul=document.getElementById('namelist');
+   ul.innerHTML=list;
 };
 
