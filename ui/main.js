e66147f4e6942243564e68e0884f233d9a1338af
ANANDHU KK <kkanandhu97@gmail.com>
2017-09-23 15:25:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ submit.onclick=function(){
     var names=['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<names.length;i++){
-        list=i +'<br/>';
+        list=+i +'<br/>';
       
   
     }
