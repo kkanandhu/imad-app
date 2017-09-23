a30b29412e09c16de9f3a09b8452da75f0043cd0
ANANDHU KK <kkanandhu97@gmail.com>
2017-09-23 13:29:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var name=nameinput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var names=['name1','name2','name3','name4'];
-    var list='ss';
+    var list='names.length';
     for(var i=0;i<names.length;i++){
         list<='<li>'+ names[i] +'</li>';
         
