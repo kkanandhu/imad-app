003ccde9bbdec2367a56e0c3ee30482636e7da6a
ANANDHU KK <kkanandhu97@gmail.com>
2017-09-23 17:09:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,14 +23,27 @@ var nameinput=document.getElementById('name');
 var name=nameinput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['name1','name2','name3','name4'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+=names[i]+"<br/> ";
-      
+    
+    
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+         if (request.readyState ==XMLHttpRequest.DONE){
+             if(request.status == 200){
+               var names=['name1','name2','name3','name4'];
+                var list='';
+            for(var i=0;i<names.length;i++){
+                 list+=names[i]+"<br/> ";
+            }
   
-    }
-    var ul=document.getElementById('namelist');
+     var ul=document.getElementById('namelist');
    ul.innerHTML=list;
+   
+             }
+         }
+    }
+
+    request.open('GET','http://kkanandhu97.imad.hasura-app.io/counter',true);
+     request.send(null);
+   
 };
 
