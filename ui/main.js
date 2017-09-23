2877bba86ba999475ed504183d0ae5530653fa90
ANANDHU KK <kkanandhu97@gmail.com>
2017-09-23 10:16:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 
 var img=document.getElementById('madi');
 img.onclik=function(){
-    document.getElementById("madi").style.top=2000 + "px"; 
+    alert("22222222222222222222222222222222222222222222222222222222222222");
+    //document.getElementById("madi").style.top=2000 + "px"; 
    //img.style.marginleft="100px" ;
 };
