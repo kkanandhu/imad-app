588bb990464a5495c2fa93c89a12fe7e95593f04
ANANDHU KK <kkanandhu97@gmail.com>
2017-09-24 12:35:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=require('pq').pool;
+var pool=require('pg').pool;
 
 var config={
         user:'kkanandhu97',
         database:'kkanandhu97',
         host:'db.imad.hasura-app.io',
         port:'5432',
-        passsword:'process.env.DB_PASSWORD',
+        passsword:'process.env.DB_PASSWORD'
         
 };
 var app = express();
