8d367db1383778b4925e9ca8c08686b15ac8c109
ANANDHU KK <kkanandhu97@gmail.com>
2017-09-24 12:45:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,15 @@ var htmltemplate=`
 return htmltemplate
 }*/
 var pool= new pool(config);
-
+app.get('/test-db', function (req, res) {
+   pool.query('select*from test',function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result)); 
+      }
+   }); 
+});
 
 
 app.get('/', function (req, res) {
